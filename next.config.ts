853594335b61f41f8ef9c03293a9f640caee46a1
Flori <fv57184@ubt-uni.net>
2025-07-26 15:41:00 +0200

    Remove static export config for Vercel

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,11 @@ import type { NextConfig } from 'next';
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  output: 'export',
-  basePath: '',
-  assetPrefix: '',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
 };
+
 
 export default nextConfig;
